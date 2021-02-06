c6d45ea6a0f1f2650e641e6764358b12434c6fa0
Fire Dragon <daniwhitlock4@gmail.com>
2021-02-06 09:59:29 -0700

    trying to figure out bug

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const express = require('express');
+
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Connect to database
 const db = new sqlite3.Database('./db/election.db', err => {
-    if (err) {
-      return console.error(err.message);
-    }
-  
-    console.log('Connected to the election database.');
-  });
+  if (err) {
+    return console.error(err.message);
+  }
+
+  console.log('Connected to the election database.');
+});
 
 
 // Default response for any other request (Not Found) Catch all 
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
-app.listen(PORT, () => {
+// Start server after DB conneciton 
+db.on('open', () => {
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+  });
 });
